54c4894814a8e756c74fac4bfc9a992bfa983877
Joseph Wang <jwang@ucvts.tec.nj.us>
2019-12-09 08:30:08 -0500

    Added getters for administrator class

--- src/com/apcsa/model/Administrator.java
+++ src/com/apcsa/model/Administrator.java
@@ -8,5 +8,37 @@ public class Administrator extends User {
     private String firstName;
     private String lastName;
     private String jobTitle;
+    
+    /**
+     * @return administratorId
+     */
+    
+    public int getAdministratorId() {
+    	return administratorId;
+    }
+    
+    /**
+     * @return firstName
+     */
+    
+    public String getFirstName() {
+    	return firstName;
+    }
+    
+    /**
+     * @return lastName
+     */
 
+    public String getLastName() {
+    	return lastName;
+    }
+    
+    /**
+     * @return jobTitle
+     */
+    
+    public String getJobTitle() {
+    	return jobTitle;
+    }
+    
 }
