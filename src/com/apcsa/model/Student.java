257c9d389ace68b7680b36678ca935781a991ae6
Matthew Man <mman@ucvts.org>
2019-12-11 08:15:40 -0500

    added getters and setters for Student file

--- src/com/apcsa/model/Student.java
+++ src/com/apcsa/model/Student.java
@@ -1,15 +1,45 @@
 package com.apcsa.model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import com.apcsa.model.User;
 
 public class Student extends User {
 
-    private int studentId;
+	public Student(int userId, String accountType, String username, String password, String lastLogin) {
+		super(userId, accountType, username, password, lastLogin);
+		// TODO Auto-generated constructor stub
+	}
+
+	private int studentId;
     private int classRank;
     private int gradeLevel;
     private int graduationYear;
     private double gpa;
     private String firstName;
     private String lastName;
-    
+
+    private int studentId() {
+		return studentId;
+    };
+    private int classRank() {
+    	return classRank;
+    };
+    private int gradeLevel() {
+    	return gradeLevel;
+    };
+    private int graduationYear() {
+    	return graduationYear;
+    };
+    private double gpa() {
+    	return gpa;
+    };
+    private String firstName() {
+    	return firstName;
+    };
+    private String lastName() {
+    	return lastName;
+    };
+
 }
