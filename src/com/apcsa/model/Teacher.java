2f525d0f04f3638b371f33d6eb03c2333a6159fc
Matthew Man <mman@ucvts.org>
2019-12-11 08:55:56 -0500

    added javadoc comments for teacher file

--- src/com/apcsa/model/Teacher.java
+++ src/com/apcsa/model/Teacher.java
@@ -12,6 +12,13 @@ public class Teacher extends User {
     private String firstName;
     private String lastName;
 
+    /**
+     * Creates an instance of the Teacher class.
+     *
+     * @param user the User
+     * @param rs a ResultSet of Teacher information
+     */
+
     public Teacher (User user, ResultSet rs) throws SQLException {
     	this(rs.getInt("teacher_id"),
              rs.getInt("department_id"),
@@ -21,6 +28,15 @@ public class Teacher extends User {
         );
     }
 
+    /**
+     * Creates an instance of the Teacher class.
+     *
+     * @param teacherId the teacher's ID
+     * @param departmentId the teacher's department
+     * @param firstName the teacher's first name
+     * @param lastName the teacher's last name
+     * @param rs a ResultSet of Teacher information
+     */
 
     public Teacher(int teacherId, int departmentId, String firstName, String lastName,  User user) {
     	super(user.getUserId(), user.getAccountType(), user.getUsername(), user.getPassword(), user.getLastLogin());
@@ -29,23 +45,35 @@ public class Teacher extends User {
 		this.firstName = firstName;
 		this.lastName = lastName;
 	}
-    
-    
+
+    /**
+     * @return teacherId
+     */
+
     public int getTeacherId() {
         return teacherId;
     }
 
-    
+    /**
+     * @return departmentId
+     */
+
     public int getDepartmentId() {
         return departmentId;
     }
-    
-    
+
+    /**
+     * @return firstName
+     */
+
     public String getFirstName() {
         return firstName;
     }
-    
-    
+
+    /**
+     * @return lastName
+     */
+
     public String getLastName() {
         return lastName;
     }
