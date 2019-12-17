7eaa4a6910c54d4119bc03d25fdfc51ec403001d
Matthew Man <mman@ucvts.org>
2019-12-17 09:02:11 -0500

    added more variables for classrank and studentid

--- src/com/apcsa/model/Student.java
+++ src/com/apcsa/model/Student.java
@@ -9,7 +9,6 @@ import com.apcsa.controller.Utils;
 import com.apcsa.data.PowerSchool;
 import com.apcsa.model.User;
 
-
 public class Student extends User {
 
     private int studentId;
@@ -19,57 +18,47 @@ public class Student extends User {
     private double gpa;
     private String firstName;
     private String lastName;
+    
+    public Student(User user, ResultSet rs) throws SQLException {
+    	super(user);
+    	
+    	this.studentId = rs.getInt("student_id");
+    	this.classRank = rs.getInt("class_rank");
+    	this.gradeLevel = rs.getInt("grade_level");
+    	this.graduationYear = rs.getInt("graduation");
+    	this.gpa = rs.getDouble("gpa");
+    	this.firstName = rs.getString("first_name");
+    	this.lastName = rs.getString("last_name");
+	}
+	
+	public Student(ResultSet rs) throws SQLException {
+		//user id, account type, username, password, last login
+		super(rs.getInt("user_id"), rs.getString("account_type"), rs.getString("username"), rs.getString("auth"), rs.getString("last_login"));
 
-    public Student (User user, ResultSet rs) throws SQLException {
-    	this(rs.getInt("student_id"),
-    		 rs.getInt("class_rank"),
-    		 rs.getInt("grade_level"),
-    		 rs.getInt("graduation"),
-    		 rs.getDouble("gpa"),
-    		 rs.getString("first_name"),
-    		 rs.getString("last_name"),
-    		 user
-    	);
-    }
-
-    public Student(int studentId, int classRank, int gradeLevel, int graduationYear, double gpa, String firstName, String lastName,
-			User user) throws SQLException {
-		super(user.getUserId(), user.getAccountType(), user.getUsername(), user.getPassword(), user.getLastLogin());
-		this.studentId = studentId;
-		this.classRank = classRank;
-		this.gradeLevel = gradeLevel;
-		this.graduationYear = graduationYear;
-		this.gpa = gpa;
-		this.firstName = firstName;
-		this.lastName = lastName;
-
+		this.studentId = rs.getInt("student_id");
+    	this.classRank = rs.getInt("class_rank");
+    	this.gradeLevel = rs.getInt("grade_level");
+    	this.graduationYear = rs.getInt("graduation");
+    	this.gpa = rs.getDouble("gpa");
+    	this.firstName = rs.getString("first_name");
+		this.lastName = rs.getString("last_name");
+	}
+    
+    public String getFirstName() {
+    	return firstName;
+	}
+	
+	public String getLastName() {
+		return this.lastName;
 	}
 
-    public int getStudentId() {
-        return studentId;
-    }
+	public int getGraduation() {
+		return this.graduationYear;
+	}
 
-    public int getClassRank() {
-        return classRank;
-    }
+	public int getGradeLevel() {
+		return this.gradeLevel;
+	}
+    
 
-    public int getGradeLevel() {
-        return gradeLevel;
-    }
-
-    public int getGraduationYear() {
-        return graduationYear;
-    }
-
-    public double getGPA() {
-        return gpa;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
 }
